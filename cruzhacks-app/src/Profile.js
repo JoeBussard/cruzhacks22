d1888104b3a2adfb73866afe7df54eb1b3000b6b
meepy bub <jbussard@ucsc.edu>
2022-01-16 00:40:38 -0800

    it can render a profile now

--- cruzhacks-app/src/Profile.js
+++ cruzhacks-app/src/Profile.js
@@ -1,19 +1,39 @@
 import React, {Component} from 'react'
+import mockDatabase from './mockDatabase.json'
 
 class Profile extends Component {
 
   render() {
-    const {profileData} = this.props;
-    const dataArrayForDisplay = profileData;
-    var firstName = profileData.firstName;
-    
+    const {userEmail} = this.props;
+    const fromDB = mockDatabase;
+    console.warn(typeof(userEmail), userEmail);
+    const userProfile = mockDatabase.users.filter(user => user.emailAddress === userEmail)
     return (
       <div className='profile'>
         <h1>Your Profile</h1>
         <div>
-          <div className='nameContainer'>{this.firstName}</div>
+          {userProfile.map(data =>{
+            return (
+              <div className="profile-container">
+                {data.firstName} {data.lastName}
+                <br/>
+                {data.headline}
+                  <h4>Interests:</h4>
+                      {data.interests.map(interest => {
+                      return (
+                        <li>{interest}</li>
+                      ) ;
+                      })}   
+                  <h4>Skills:</h4>
+                      {data.skills.map(skill => {
+                      return (
+                        <li>{skill}</li>
+                      ) ;
+                      })}
+              </div>
+            )
+          })}
         </div>
-        <div>{JSON.stringify(dataArrayForDisplay)}</div>
       </div>
     )
   }
