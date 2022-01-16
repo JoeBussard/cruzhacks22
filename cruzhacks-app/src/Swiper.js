158f1d2a01034d0c4f8e1368aa9f6502a947807b
meepy bub <jbussard@ucsc.edu>
2022-01-16 02:47:27 -0800

    added swiper pic

---
+++ cruzhacks-app/src/Swiper.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import mockDatabase from './mockDatabase.json'
+import Profile from './Profile.js'
+
+class Swiper extends Component {
+
+
+
+  render () {
+
+
+  }
+}
+
+export default Swiper
